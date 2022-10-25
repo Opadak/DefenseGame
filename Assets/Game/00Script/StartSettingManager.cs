6c2406cc99c06aacc607f93fb5069356febc6661
Opadak <72779772+Opadak@users.noreply.github.com>
2022-10-25 09:17:07 +0900

    2022-10-25
    Set up Dotween

---
+++ Assets/Game/00Script/StartSettingManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSettingManager : MonoBehaviour
+{
+    private float mapX = 0f;
+    private float mapY = 4f;
+
+  
+    void Start()
+    {
+        
+    }
+    void MapSetting()
+    {
+       for (int i = 0; i < 10; i++)
+        {
+            
+        }
+    }
+   
+}
